2496e5b475eb9e400b365c98833d530792fd091e
Sergey Shein <sergeyshein20@gmail.com>
2020-12-29 16:53:57 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price_ticket = 7317;
+        int fixed_amount = 20;
+        int mile = price_ticket / fixed_amount;
+        System.out.println(mile);
+    }
+}
